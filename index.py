c69b7c7d31210c9c38332e15f70782fcf348b955
Elvan Owen <vanzz_95@hotmail.com>
2016-11-26 18:13:42 +0700

    Added pass to stub

--- index.py
+++ index.py
@@ -45,13 +45,14 @@ try:
 
         # Calculate Ratings based on already obtained tweets
         # Calculation is done real time, based on the number of tweets fetched
-        if numberOfTweets % 100 == 0:
+        if (numberOfTweets % 100) == 0:
             # Code
             # Code
             # Code
 
             # Print Movie ratings
             # print(movie.title() + " Rating : " + rating, end='\r')
+            pass
 
 except TwitterSearchException as e: # take care of all those ugly errors if there are some
     print(e)
